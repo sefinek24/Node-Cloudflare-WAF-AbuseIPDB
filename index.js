d7b05484f5b51e0f44d7f0869e1177c13b0caf12
Sefinek <contact@sefinek.net>
2024-08-19 11:52:17 +0200

    Sort variables

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const { logToCSV, readReportedIPs, wasImageRequestLogged } = require('./scripts/
 const formatDelay = require('./scripts/formatDelay.js');
 const log = require('./scripts/log.js');
 
-const COOLDOWN_MS = 2000;
-const REPORTED_IP_COOLDOWN_MS = 7 * 60 * 60 * 1000;
-const MAX_URL_LENGTH = 2000;
 const MAIN_DELAY = process.env.NODE_ENV === 'production' ? 4 * 60 * 60 * 1000 : 8 * 1000;
+const REPORTED_IP_COOLDOWN_MS = 7 * 60 * 60 * 1000;
+const COOLDOWN_MS = 2000;
+const MAX_URL_LENGTH = 2000;
 
 const fetchBlockedIPs = async () => {
 	try {
