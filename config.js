f6b96f38ff8b3fcaeece22e975e9e621b9b74e83
Sefinek <contact@sefinek.net>
2024-09-18 12:32:05 +0200

    Add default values

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CYCLE_INTERVAL = process.env.NODE_ENV === 'production' ?
-	parseInt(process.env.CYCLE_INTERVAL) * 60 * 1000 : 8 * 1000;
+	parseInt(process.env.CYCLE_INTERVAL || '120') * 60 * 1000 : 8 * 1000;
 
-const REPORTED_IP_COOLDOWN_MS = parseInt(process.env.REPORTED_IP_COOLDOWN_MS) * 60 * 60 * 1000;
+const REPORTED_IP_COOLDOWN_MS = parseInt(process.env.REPORTED_IP_COOLDOWN_MS || '6') * 60 * 60 * 1000;
 
-const MAX_URL_LENGTH = parseInt(process.env.MAX_URL_LENGTH);
+const MAX_URL_LENGTH = parseInt(process.env.MAX_URL_LENGTH || '920');
 
-const SUCCESS_COOLDOWN_MS = parseInt(process.env.SUCCESS_COOLDOWN_MS);
+const SUCCESS_COOLDOWN_MS = parseInt(process.env.SUCCESS_COOLDOWN_MS || '2') * 1000;
 
-const IP_REFRESH_INTERVAL = parseInt(process.env.IP_REFRESH_INTERVAL) * 60 * 1000;
+const IP_REFRESH_INTERVAL = parseInt(process.env.IP_REFRESH_INTERVAL || '80') * 60 * 1000;
 
 const REPORT_TO_SEFINEK_API = process.env.REPORT_TO_SEFINEK_API === 'true';
 
 const SEFINEK_API_INTERVAL = process.env.NODE_ENV === 'production' ?
-	parseInt(process.env.SEFINEK_API_INTERVAL) * 60 * 1000 : 5 * 1000;
+	parseInt(process.env.SEFINEK_API_INTERVAL || '1') * 60 * 60 * 1000 : 5 * 1000;
 
 module.exports = {
 	CYCLE_INTERVAL,
